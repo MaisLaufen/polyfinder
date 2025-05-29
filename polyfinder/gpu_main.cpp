e6554bee2fb2ad1095e2f7e83e65f4515b5fcb0b
MaisLaufen <vip.eftifanov@mail.ru>
2025-05-30 00:24:37 +0700

    [Add] ST and MT methods for gpu

--- polyfinder/gpu_main.cpp
+++ polyfinder/gpu_main.cpp
@@ -1,10 +1,15 @@
 #include "gpu_main.h";
 
-void gpu_main(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end) {
+void gpu_main_multi_poly(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end) {
     printf("gpu_main start.\n");
-    for (unsigned int i = m_start; i <= m_end; ++i) {
-        for (unsigned int j = t_start; j <= t_end; ++j) {
-            polyfinder_gpu my(i, j);
+    for (unsigned int m = m_start; m <= m_end; ++m) {
+        for (unsigned int t = t_start; t <= t_end; ++t) {
+            polyfinder_gpu my(m, t);
         }
     }
+}
+
+
+void gpu_main_single_poly(unsigned int m, unsigned int t) {
+    polyfinder_gpu my(m, t);
 }
--- polyfinder/gpu_main.h
+++ polyfinder/gpu_main.h
@@ -2,6 +2,7 @@
 #define GPU_MAIN_H
 #include "polyfinder_gpu.h"
 
-void gpu_main(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end);
+void gpu_main_multi_poly(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end);
+void gpu_main_single_poly(unsigned int m, unsigned int t);
 
 #endif

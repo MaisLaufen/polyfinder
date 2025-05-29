1e7e9a2a78a944aceeac44c11519edb76bcf4eaf
MaisLaufen <vip.eftifanov@mail.ru>
2025-05-30 00:24:22 +0700

    [Add] ST and MT methods for cpu

--- polyfinder/cpu_main.cpp
+++ polyfinder/cpu_main.cpp
@@ -1,9 +1,15 @@
 #include "cpu_main.h";
 
-void cpu_main(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end) {
-    for (unsigned int i = m_start; i <= m_end; ++i) {
-        for (unsigned int j = t_start; j <= t_end; ++j) {
-            polyfinder my(i, j);
+void cpu_main_multi_poly(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end) {
+    for (unsigned int m = m_start; m <= m_end; ++m) {
+        for (unsigned int t = t_start; t <= t_end; ++t) {
+            polyfinder my(m, t);
+            long long result = my.find_cyclic_polynom();
+            printf("%llu ", result); printf(" poly\n");
+
         }
     }
+}
+void cpu_main_single_poly(unsigned int m, unsigned int t) {
+    polyfinder my(m, t);
 }
--- polyfinder/cpu_main.h
+++ polyfinder/cpu_main.h
@@ -2,6 +2,7 @@
 #define CPU_MAIN_H
 #include "polyfinder.h"
 
-void cpu_main(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end);
+void cpu_main_multi_poly(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end);
+void cpu_main_single_poly(unsigned int m, unsigned int t);
 
 #endif

768fbefcde8d0c3589c552951502df64dbc4a7ba
MaisLaufen <vip.eftifanov@mail.ru>
2025-06-02 00:44:02 +0700

    [Fix] Transfer gpu main for streaming

--- polyfinder/gpu_main.cpp
+++ polyfinder/gpu_main.cpp
@@ -1,15 +1,63 @@
 #include "gpu_main.h";
 
-void gpu_main_multi_poly(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end) {
-    printf("gpu_main start.\n");
+json gpu_main_multi_poly(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end) {
+    json result_json = json::array();
+
     for (unsigned int m = m_start; m <= m_end; ++m) {
         for (unsigned int t = t_start; t <= t_end; ++t) {
-            polyfinder_gpu my(m, t);
+            polyfinder_gpu pf(m, t);
+            PolyfinderResult result = pf.find_cyclic_polynom_gpu();
+
+            if (result.polynom != -1) {
+                result_json.push_back({
+                    {"m", m},
+                    {"t", t},
+                    {"polynom", result.polynom},
+                    {"iterations", result.iterations},
+                    {"time_seconds", result.time_seconds}
+                    });
+            }
+            else {
+                result_json.push_back({
+                    {"m", m},
+                    {"t", t},
+                    {"polynom", nullptr},
+                    {"iterations", result.iterations},
+                    {"time_seconds", result.time_seconds},
+                    {"error", "Cyclic polynomial not found"}
+                    });
+            }
         }
     }
+
+    return result_json;
 }
 
+json gpu_main_single_poly(unsigned int m, unsigned int t) {
+    json result_json = json::array();
 
-void gpu_main_single_poly(unsigned int m, unsigned int t) {
-    polyfinder_gpu my(m, t);
+    polyfinder_gpu pf(m, t);
+    PolyfinderResult result = pf.find_cyclic_polynom_gpu();
+
+    if (result.polynom != -1) {
+        result_json.push_back({
+            {"m", m},
+            {"t", t},
+            {"polynom", result.polynom},
+            {"iterations", result.iterations},
+            {"time_seconds", result.time_seconds}
+            });
+    }
+    else {
+        result_json.push_back({
+            {"m", m},
+            {"t", t},
+            {"polynom", nullptr},
+            {"iterations", result.iterations},
+            {"time_seconds", result.time_seconds},
+            {"error", "Cyclic polynomial not found"}
+            });
+    }
+
+    return result_json;
 }
--- polyfinder/gpu_main.h
+++ polyfinder/gpu_main.h
@@ -1,8 +1,13 @@
+#pragma once
+
 #ifndef GPU_MAIN_H
 #define GPU_MAIN_H
 #include "polyfinder_gpu.h"
+#include <nlohmann/json.hpp>
 
-void gpu_main_multi_poly(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end);
-void gpu_main_single_poly(unsigned int m, unsigned int t);
+using json = nlohmann::json;
+
+json gpu_main_multi_poly(unsigned int m_start, unsigned int m_end, unsigned int t_start, unsigned int t_end);
+json gpu_main_single_poly(unsigned int m, unsigned int t);
 
 #endif
